fb073d94eba08ab2c8822eb8844bb9ce7e990987
Roman kebede <romankebede69@gmail.com>
2024-05-31 13:44:58 +0000

    Go_route navigation

--- personal_health_tracker/lib/presention/screens/auth/bloc/auth_bloc.dart
+++ personal_health_tracker/lib/presention/screens/auth/bloc/auth_bloc.dart
@@ -15,6 +15,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     on<LogInEvent>(logInEvent);
     on<NavigateToLoginPageEvent>(navigateToLoginEvent);
     on<SignupUserEvent>(signupUserEvent);
+    on<AdminLogEvent>(adminLogEvent);
   }
 
   FutureOr<void> signUpEvent(
@@ -27,7 +28,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   }
 
   FutureOr<void> logInEvent(LogInEvent event, Emitter<AuthState> emit) async {
-    String uri = 'http://localhost:3000/auth/signin';
+    String uri = 'http://localhost:3000/auth/login';
     final url = Uri.parse(uri);
 
     final body = {"username": event.useremail, "password": event.password};
@@ -99,4 +100,34 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   
 
   
+
+  FutureOr<void> adminLogEvent(AdminLogEvent event, Emitter<AuthState> emit) async {
+    String uri = 'http://localhost:3000/auth/login';
+    final url = Uri.parse(uri);
+
+    final body = {"username": event.useremail, "password": event.password};
+    final jsonBody = jsonEncode(body);
+    final headers = {"Content-Type": "application/json"};
+
+    final res = await http.post(url, headers: headers, body: jsonBody);
+    Map response = jsonDecode(res.body);
+    print(response);
+    if (response.containsKey('message')) {
+      var error = response["message"];
+
+      if (error is! String) {
+        error = error[0];
+      }
+      emit(AdminLogErrorState(error: error));
+    } else {
+      final secureStorage = SecureStorage().secureStorage;
+
+      await secureStorage.write(key: "role", value: response["role"]);
+      await secureStorage.write(key: "token", value: response["accessToken"]);
+      await secureStorage.write(key: "username", value: response["username"]);
+      await secureStorage.write(key: "email", value: response["email"]);
+
+      emit(AdminLogSuccessState());
+    }
+  }
 }
